4d76b424eb9696a65733201382db256c9d3e6097
Felana42 <rafaliarisoafelana@gmail.com>
2026-07-20 15:13:44 +0300

    Update traitement_inscription.php

--- traitement_inscription.php
+++ traitement_inscription.php
@@ -4,7 +4,9 @@ include("fonction.php");
 $nom = $_POST['nom'];
 $numero_etu = $_POST['numero_etu'];
 
-insert_membre($nom,$numero_etu);
 
-header("Location:accueil.php");
+$nouvel_id = insert_membre($nom,$numero_etu);
+
+header("Location:accueil.php?id_membre=" .$nouvel_id);
+exit();
 ?>

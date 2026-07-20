946c09c447a8d8aa3f767fbae6840f6a252bf6ae
Felana42 <rafaliarisoafelana@gmail.com>
2026-07-20 16:53:21 +0300

    Create statCat.php

---
+++ statCat.php
@@ -0,0 +1,32 @@
+<?php 
+include "fonction.php";
+
+
+$cat = getStatCategorie();
+var_dump($cat);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Statistiques par catégorie</h1>
+
+<?php foreach($cat as $c){ ?>
+
+    <p>
+        <a href="statPro.php?id_categorie=<?php echo $c['id_categorie']; ?>">
+            <?php echo $c['nom_categorie']; ?>
+        </a>
+
+        : <?php echo $c['total']; ?> ventes
+    </p>
+
+<?php } ?>
+
+</body>
+</html>

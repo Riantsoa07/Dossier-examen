540cb0a601259150fe13cb3ac0385192e6bc8582
Riantsoa07 <idariantsoamamizo@gmail.com>
2026-07-21 12:37:36 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,46 @@
 <?php 
 include "fonction.php";
-
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <h1>Login</h1>
+
+<div class="container mt-5">
+
+    <h1 class="mb-4">Login</h1>
+
     <form action="traitement.php" method="post">
-        Numero Etudiant:
-        <input type="text" name="numero_etu" placeholder="ETU001">
-        <p><input type="submit" value="Se connecter"></p>
+
+        <div class="mb-3">
+            <label class="form-label">
+                Numero Etudiant:
+            </label>
+
+            <input 
+                type="text" 
+                name="numero_etu" 
+                class="form-control"
+                placeholder="ETU001">
+        </div>
+
+
+        <input 
+            type="submit" 
+            value="Se connecter"
+            class="btn btn-primary">
+
     </form>
+
+</div>
+
 </body>
 </html>

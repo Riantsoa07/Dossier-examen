f8c270e7d9797e77aefddc91aecf31e5d1e96cb8
Riantsoa07 <idariantsoamamizo@gmail.com>
2026-07-21 12:38:20 +0300

    Update vendre.php

--- vendre.php
+++ vendre.php
@@ -13,7 +13,7 @@ $liste = getListeProduits()
 </head>
 <body>
     <h2>Vente de produits </h2>
-    <form action="traitement_vendre.php" method="post">
+    <form action="traitement_vendre.php" method="post" enctype="multipart/form-data">
         <input type="hidden" name="id_membre" value="<?php echo $id_membre; ?>">
 
        <p> Produit : <select name="id_produit">
@@ -26,6 +26,10 @@ $liste = getListeProduits()
         <p>Prix de vente: <input type="number" name="prix_vente"></p>
         <p>Quantite: <input type="number" name="quantite_dispo"></p>
         <p>Date disponible: <input type="date" name="date_dispo"></p>
+        <p>
+        Photo :
+        <input type="file" name="photo">
+        </p>
         <input type="submit" value="Valider">
     </form>
 </body>

bb06e6bbe76a8b72dc67b8332146cbb0e5f46a9e
Riantsoa07 <idariantsoamamizo@gmail.com>
2026-07-21 12:38:39 +0300

    Update traitement_vendre.php

--- traitement_vendre.php
+++ traitement_vendre.php
@@ -6,7 +6,28 @@ $id_membre = $_POST['id_membre'];
 $prix_vente = $_POST['prix_vente'];
 $quantite_dispo = $_POST['quantite_dispo'];
 $date_dispo = $_POST['date_dispo'];
-vendreProduits($id_produit,$id_membre,$prix_vente,$quantite_dispo,$date_dispo);
+//photo
+$photo = null;
+
+if(isset($_FILES['photo']) && $_FILES['photo']['name'] != ""){
+
+    $photo = $_FILES['photo']['name'];
+
+    move_uploaded_file(
+        $_FILES['photo']['tmp_name'],
+        "images/".$photo
+    );
+}
+
+
+vendreProduits(
+    $id_produit,
+    $id_membre,
+    $prix_vente,
+    $quantite_dispo,
+    $date_dispo,
+    $photo
+);
 
 header("Location: accueil.php?id_membre=".$id_membre);
 ?>
